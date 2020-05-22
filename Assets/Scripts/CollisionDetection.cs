ae5fd285906f0886421c182791b55da4eaaef399
Alex <pg18alejandro@vfs.com>
2020-05-21 20:37:03 -0700

    ScoreAndCollisions

--- Assets/Scripts/CollisionDetection.cs
+++ Assets/Scripts/CollisionDetection.cs
@@ -1,15 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 [RequireComponent(typeof(Collider2D))]
 public class CollisionDetection : MonoBehaviour
 {
+    [SerializeField]
+    private GameDirector _gDirector;
+
     private void OnCollisionEnter(Collision other)
     {
         if(other.gameObject.CompareTag("Obstacle"))
         {
+            _gDirector.Death();
+        }
 
+        if(other.gameObject.CompareTag("CheckPoint"))
+        {
+            _gDirector.AddPoints(1);
         }
     }
 }
