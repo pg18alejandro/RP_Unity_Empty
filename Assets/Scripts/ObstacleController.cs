505f9611b3d70355f53665f375d1fa5dd3e41876
Jonathan Damiani <jdeandamianiapps@gmail.com>
2020-05-21 20:12:31 -0700

    obstacles

---
+++ Assets/Scripts/ObstacleController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleController : MonoBehaviour
+{
+    public GameObject pipe;
+
+    private Vector3 lastPosition;
+
+    private void Start()
+    {
+        lastPosition =  new Vector3(1,0,0);
+        InvokeRepeating("AddObstacle", 0, 1f);
+    }
+
+
+    private void AddObstacle()
+    {
+        lastPosition.y = Random.Range(-0.5f, 6.5f);
+        GameObject newPipe = Instantiate(pipe, lastPosition, Quaternion.identity);
+        lastPosition.x += 1.5f;
+    }
+
+}
